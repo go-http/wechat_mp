278928feab43a17a7fb7b21f8dda30b3b6b0b276
Athurg Feng <hi@atfeng.com>
2017-08-25 17:01:27 +0800

    增加模板消息的发送功能

---
+++ message_template.go
@@ -0,0 +1,54 @@
+package weixin
+
+import (
+	"bytes"
+	"encoding/json"
+)
+
+type TemplateMessageData struct {
+	Value string `json:"value"`
+	Color string `json:"color"` //#rrggbb
+}
+
+type MiniProgram struct {
+	Appid    string `json:"appid,omitempty"`
+	PagePath string `json:"pagepath,omitempty"`
+}
+
+type TemplateMessage struct {
+	ToUser      string                         `json:"touser"` //收件人的OPENID
+	TemplateId  string                         `json:"template_id"`
+	Url         string                         `json:"url,omitempty"`
+	MiniProgram *MiniProgram                   `json:"miniprogram,omitempty"`
+	Data        map[string]TemplateMessageData `json:"data,omitempty"`
+}
+
+func (client *Client) SendTemplateMessage(msg *TemplateMessage) (int, error) {
+	reqBytes, _ := json.Marshal(msg)
+
+	var resp struct {
+		BaseResponse
+		MsgId int
+	}
+
+	err := client.request("POST", "/message/template/send", nil, bytes.NewBuffer(reqBytes), &resp)
+
+	return resp.MsgId, err
+}
+
+func NewTemplateMessage(templateId, toOpenId string) *TemplateMessage {
+	return &TemplateMessage{
+		ToUser:     toOpenId,
+		TemplateId: templateId,
+		Data:       make(map[string]TemplateMessageData),
+	}
+}
+
+func (msg *TemplateMessage) SetData(key, value, color string) {
+	data := TemplateMessageData{Value: value, Color: color}
+	msg.Data[key] = data
+}
+
+func (msg *TemplateMessage) SetMiniProgram(appId, pagePath string) {
+	msg.MiniProgram = &MiniProgram{Appid: appId, PagePath: pagePath}
+}
