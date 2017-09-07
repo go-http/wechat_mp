e09e564deeafa5899872290ee18887b583938590
Athurg Feng <hi@atfeng.com>
2017-09-07 15:24:09 +0800

    增加下载图片消息中包含的图片的功能

--- xml_message.go
+++ xml_message.go
@@ -2,6 +2,9 @@ package weixin
 
 import (
 	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"net/http"
 )
 
 //用<![CDATA[和]]包裹的文本
@@ -32,4 +35,32 @@ type XMLMessage struct {
 			PicMd5Sum CDATA
 		} `xml:"PicList>item"`
 	}
+}
+
+func (msg XMLMessage) SaveImageTo(path string) error {
+	if msg.MsgType.String != "image" {
+		return fmt.Errorf("非图片消息")
+	}
+
+	if msg.PicUrl.String == "" {
+		return fmt.Errorf("图片地址为空")
+	}
+
+	resp, err := http.Get(msg.PicUrl.String)
+	if err != nil {
+		return fmt.Errorf("请求错误: %s", err)
+	}
+	defer resp.Body.Close()
+
+	buffer, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return fmt.Errorf("读取错误: %s", err)
+	}
+
+	err = ioutil.WriteFile(path, buffer, 0755)
+	if err != nil {
+		return fmt.Errorf("保存错误: %s", err)
+	}
+
+	return nil
 }
